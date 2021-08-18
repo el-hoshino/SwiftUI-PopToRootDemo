de087288037e8edcf143e6067729c9b1dc2512a7
史 翔新 <x_shi@yumemi.co.jp>
2021-08-19 01:37:29 +0900

    Init project

---
+++ SwiftUI-PopToRootDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-PopToRootDemo
+//
+//  Created by 史 翔新 on 2021/08/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-PopToRootDemo/SwiftUI_PopToRootDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PopToRootDemoApp.swift
+//  SwiftUI-PopToRootDemo
+//
+//  Created by 史 翔新 on 2021/08/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PopToRootDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
